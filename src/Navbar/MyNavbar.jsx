15aa8fe45b3809ca4b50a4e2f2c8eeef977f6552
AllisonWonderland <aneary@gmail.com>
2021-05-18 18:27:50 -0500

    addeded nav bar file

---
+++ src/Navbar/MyNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './MyNavbar.css';
+
+
+function MyNavbar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MyNavbar
