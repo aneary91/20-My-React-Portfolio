e8201e8e20e39851136bb47829578cab23ced1ce
AllisonWonderland <aneary@gmail.com>
2021-05-22 12:52:33 -0500

    imported themeprovider and theme.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import MyNavbar from './components/Navbar/MyNavbar';
 
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+    My Portfolio
     <MyNavbar />
     
     </div>
