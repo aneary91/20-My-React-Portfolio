cfab74e5f6263e563cd3d46c91e1842540972366
AllisonWonderland <aneary@gmail.com>
2021-05-24 09:07:19 -0500

    added themes and more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import MyNavbar from "./components/Navbar/MyNavbar";
+import TitleMessage from "./components/TitleMessage/MyTitleMessage.jsx";
 
 const App = () => {
   return (
     <div className="App">
       My Portfolio
       <MyNavbar />
+      <TitleMessage />
+      <myTitleMessage/>
     </div>
   );
 };
